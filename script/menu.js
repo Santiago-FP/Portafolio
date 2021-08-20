9e9d9c8f8b563e38e392891a797a75de66464db3
Santiago Flores <sanffp@hotmail.com>
2021-08-20 05:36:27 -0500

    Se añadio js al menu en pantalla pequeña

--- script/menu.js
+++ script/menu.js
@@ -3,8 +3,11 @@ function function1(){
     alert("hola");
     // menuButton.style.color = "yellow"
 }
+function showNav(){
+    nav.classList.toggle("showNav");    
+}
 
 const menuButton = document.getElementById("menu");
 const nav = document.getElementById("nav");
 
-menuButton.onclick  = function1;
+menuButton.onclick  = showNav;
